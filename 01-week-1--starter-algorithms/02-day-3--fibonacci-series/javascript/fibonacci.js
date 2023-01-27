449edbf901125c2816c02606ccecb33304fb3b9c
Joe <spizjo01@gmail.com>
2023-01-27 13:11:16 -0500

    tests for fib passed

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,7 +1,16 @@
 function fibonacci(num) {
-  // type your code here
-}
-
+  let fib = [0,1]
+  for (let i = 3; num+1 > fib.length; i++) {
+      let next = fib[(i-3)]+fib[(i-2)]
+      fib.push(next)
+     }
+  return(fib[num])
+ }
+//creating fibb sequence: create array. start array with 0,1. 
+// i at n spot would be n-1 + n-2 = ___
+//build sequence with while loop. 
+//while length of array is more than while loopp variable keep pushing 
+//fib seq. when done. print nth i
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
