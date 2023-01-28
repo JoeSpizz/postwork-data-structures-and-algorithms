1813f3bfb1a2683e48bde630dbfc8cab19f3d265
Joe <spizjo01@gmail.com>
2023-01-28 10:49:05 -0500

    find shortest string completed

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,7 +1,17 @@
 function findShortestString(arr) {
-  // type your code here
-}
+ let shortest = arr[0];
 
+  arr.map(string => {
+    if (string.length < shortest.length) {
+      shortest = string;
+    }
+  });
+
+  return shortest;
+ }
+//examine each element in the array. use .length to determine 
+//string length. store first string in variable. ONLY If .length of 
+//next element is less, replace variable. if not move on.
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
@@ -18,6 +28,14 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  let startTime = Date.now()
+
+  for (let i = 0; i < 1000; i++) {
+    let strings = ["hello", "goodbye", "orange", "hard", "confused"]
+    findShortestString(strings)
+  }
+  let time = (Date.now() - startTime )/ 1000
+  console.log(time)
 }
 
 module.exports = findShortestString;
