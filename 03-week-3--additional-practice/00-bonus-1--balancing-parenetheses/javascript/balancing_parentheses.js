606f3b2ab1ee81289283bb397139ba1ddd3c0bd7
Joe <spizjo01@gmail.com>
2023-02-01 15:22:01 -0700

    balancing parenthesis javascript passed

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -1,7 +1,25 @@
 function balancingParentheses(string) {
   // type your code here
-}
+  let openP= 0
+  let missing = 0
+  for (let i = 0; i < string.length; i++) {
+    if(string[i]==='('){
+      openP++
+     continue
+    }
 
+    if(openP>0){
+     openP--
+    }else{
+      missing++
+    }
+    
+  }
+  return openP + missing
+}
+//for each '(' there must be a ')'
+//we need to iterate through every character. if '(' then 
+//look for ')'. delete them?  
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
